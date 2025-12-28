a66aec4f79b6a47c631c9975d48592d68163dddf
nadilakencana <nadilakencana1107@gmail.com>
2025-12-28 20:03:58 +0700

    feat: add swr for using loading facth data

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,30 +1,36 @@
+import { fetcher } from "@/utils/fetcher/fetcher";
 import ProductView from "@/views/Product";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-
+import useSWR from "swr";
 
 
 const ProductPage = () =>{
 
     const[isLogin, setIsLogin] = useState(true);
-    const[products, setProducts] = useState([]);
+    // const[products, setProducts] = useState([]);
     const{ push } = useRouter();
-    console.log(products);
+    // console.log(products);
     useEffect(() => {
         if(!isLogin){
             push("/auth/login")
         }
     }, [])
 
-    useEffect(() => {
-        fetch("/api/product")
-        .then((res) => res.json())
-        .then((data) => setProducts(data.data))
-    }, [])  
+    const { data, error, isLoading } = useSWR("/api/product", fetcher);
+    console.log(data);
+    console.log(error);
+    console.log(isLoading);
+
+    // useEffect(() => {
+    //     fetch("/api/product")
+    //     .then((res) => res.json())
+    //     .then((data) => setProducts(data.data))
+    // }, [])  
 
     return (
         <div>
-           <ProductView products={products}/>
+           <ProductView products={isLoading ? [] : data.data}/>
                 
         </div>
     )
--- src/views/Product/index.tsx
+++ src/views/Product/index.tsx
@@ -15,21 +15,33 @@ const ProductView = ({products} : {products: productType[]}) =>{
     return(
          <div className={style.product}>
             <h2 className={style.product__title}>Product</h2>
-            <div className={style.product__list}>
-                {products.map((product: productType) => (
-                    <div key={product.id} className={style.product__list__item}>
-                        <div className={style.product__list__item__image}>
-                            <img src={product.image} alt={product.name} />
+            < div className={style.product__list}>
+            {products.length > 0 ? (
+                <>
+                    {products.map((product: productType) => (
+                        <div key={product.id} className={style.product__list__item}>
+                            <div className={style.product__list__item__image}>
+                                <img src={product.image} alt={product.name} />
+                            </div>
+                            <h4 className={style.product__list__item__name}>{product.name}</h4> 
+                            <p className={style.product__list__item__category}>{product.categories}</p>
+                            <p className={style.product__list__item__price}>{product.price.toLocaleString('id-ID',{
+                                style: 'currency',
+                                currency: 'IDR',
+                                minimumFractionDigits: 0
+                            })}</p>
                         </div>
-                        <h4 className={style.product__list__item__name}>{product.name}</h4> 
-                        <p className={style.product__list__item__category}>{product.categories}</p>
-                        <p className={style.product__list__item__price}>{product.price.toLocaleString('id-ID',{
-                            style: 'currency',
-                            currency: 'IDR',
-                            minimumFractionDigits: 0
-                        })}</p>
-                    </div>
-                ))}
+                    ))}
+                </>
+            ) : (
+                <div className={style.product__list__skeleton}>
+                    <div className={style.product__list__skeleton__image}/>
+                    <div className={style.product__list__skeleton__name}/>
+                    <div className={style.product__list__skeleton__category}/>
+                    <div className={style.product__list__skeleton__price}/>
+                </div>
+            )}
+                
             </div>
                 
         </div>
